cd0d0ecf98eb2930143c124426e8b03270a9280f
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-05-14 04:17:30 +0300

    color update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='bg-black text-white'>App</div>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-background dark:bg-background text-text">
+      Navbar Section
+    </div>
+  );
+};
+
+export default Navbar;
