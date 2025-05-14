ff59d4b2f993168c33e9bf08b0b17cb35020a869
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-05-14 05:18:52 +0300

    NEUR-10 | navbar essentials

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,45 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="bg-background dark:bg-background text-text">
-      Navbar Section
-    </div>
+    <nav className="px-16 py-5 flex items-center justify-between font-medium">
+      {/* logo */}
+      <a href="/">
+        <img src={Logo} alt="logo" className="w-42" />
+      </a>
+
+      {/* navItems */}
+      <ul className="flex gap-9">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Features</a>
+        </li>
+        <li>
+          <a href="#">Pricing</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      {/* buttons */}
+      <ul className=" flex gap-[18px]">
+        <li>
+          <a
+            href="#"
+            className="px-6 py-3 bg-secondary hover:bg-hover-secondary transition text-white rounded-full"
+          >
+            Login
+          </a>
+        </li>
+        <li>
+          <a href="#">Learn More</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
